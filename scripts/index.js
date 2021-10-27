e34602ccbffd0e42bb7c53d4adcb1c7c584bd287
Mikhail KUKRESH <kukresh_m@M11highway.pro>
2021-10-27 22:27:16 +0300

    worcPupap

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,42 @@
-console.log("Привет мир");
+
 const popupElement = document.querySelector(".popup");
-console.log(popupElement);
+
 const popupCloseButtonElement = popupElement.querySelector(".popup__close");
-console.log(popupCloseButtonElement);
+
 const profileOpenPopupButton = document.querySelector(".profile__open-popup");
-console.log(profileOpenPopupButton);
+
+let nameProfile = document.querySelector('.profile__name');
+
+let namePopup = document.querySelector('.popup__name');
+
+namePopup.value = nameProfile.textContent;
+
+let professionProfile = document.querySelector('.profile__profession');
+
+let professionPopup = document.querySelector('.popup__profession');
+
+professionPopup.value = professionProfile.textContent;
 
 
-const toglPopupVisiboliti = function() {
-   popupElement.classList.toggle('popup__is-open'); 
-
+const openPopup = function() {
+   popupElement.classList.add('popup__is-open');   
 } 
 
-toglPopupVisiboliti();
+const closePopup = function() {
+   popupElement.classList.remove('popup__is-open'); 
+} 
+
+profileOpenPopupButton.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+
+function formSubmitHandler (evt) {
+   evt.preventDefault(); 
+   nameProfile.textContent = namePopup.value;
+   professionProfile.textContent = professionPopup.value;
+      closePopup();
+};
+
+const popupSaveBotton = popupElement.querySelector('.form__element');
+console.log(popupSaveBotton);
+
+popupSaveBotton.addEventListener('submit', formSubmitHandler); 
