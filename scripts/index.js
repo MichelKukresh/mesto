f8ef1857371dde899f40d56a8106ea554c9358b5
Mikhail KUKRESH <kukresh_m@M11highway.pro>
2021-10-27 23:28:50 +0300

    addnojekyll

--- scripts/index.js
+++ scripts/index.js
@@ -1,42 +1,40 @@
-
 const popupElement = document.querySelector(".popup");
 
 const popupCloseButtonElement = popupElement.querySelector(".popup__close");
 
 const profileOpenPopupButton = document.querySelector(".profile__open-popup");
 
-let nameProfile = document.querySelector('.profile__name');
+let nameProfile = document.querySelector(".profile__name");
 
-let namePopup = document.querySelector('.popup__name');
+let namePopup = document.querySelector(".popup__name");
 
 namePopup.value = nameProfile.textContent;
 
-let professionProfile = document.querySelector('.profile__profession');
+let professionProfile = document.querySelector(".profile__profession");
 
-let professionPopup = document.querySelector('.popup__profession');
+let professionPopup = document.querySelector(".popup__profession");
 
 professionPopup.value = professionProfile.textContent;
 
-
-const openPopup = function() {
-   popupElement.classList.add('popup__is-open');   
-} 
-
-const closePopup = function() {
-   popupElement.classList.remove('popup__is-open'); 
-} 
-
-profileOpenPopupButton.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
-
-function formSubmitHandler (evt) {
-   evt.preventDefault(); 
-   nameProfile.textContent = namePopup.value;
-   professionProfile.textContent = professionPopup.value;
-      closePopup();
+const openPopup = function () {
+  popupElement.classList.add("popup__is-open");
 };
 
-const popupSaveBotton = popupElement.querySelector('.form__element');
+const closePopup = function () {
+  popupElement.classList.remove("popup__is-open");
+};
+
+profileOpenPopupButton.addEventListener("click", openPopup);
+popupCloseButtonElement.addEventListener("click", closePopup);
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  nameProfile.textContent = namePopup.value;
+  professionProfile.textContent = professionPopup.value;
+  closePopup();
+}
+
+const popupSaveBotton = popupElement.querySelector(".form__element");
 console.log(popupSaveBotton);
 
-popupSaveBotton.addEventListener('submit', formSubmitHandler); 
+popupSaveBotton.addEventListener("submit", formSubmitHandler);
