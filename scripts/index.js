569563b7ff7146926c6fb9b63757ccf514cea4c7
Mikhail KUKRESH <kukresh_m@M11highway.pro>
2021-11-13 12:51:26 +0300

    worcsize

--- scripts/index.js
+++ scripts/index.js
@@ -98,26 +98,34 @@ function addCard(name, link) {
 	htmlElement.querySelector('.elements__cut-text').innerText = name;
   htmlElement.querySelector('#image-element').src = link;
 
-	//2.5 Навесить события
-	// ПОЗЖЕ //setListeners(htmlElement);
+
+	
 //организация лайка
   htmlElement.querySelector(".elements__hart").addEventListener('click', function(evt) { 
   evt.target.classList.toggle("elements__hart_activ");
   });
 
+//2.5 Навесить события
+  setListener(htmlElement);
+
+
+  //организация открытия большой картинки
+  htmlElement.querySelector('#image-element').addEventListener('click', openSize);
+
 
 	//3. Вставлять разметку в наш dom
 	elementsItem.prepend(htmlElement);//все передал на отрисовку вместе с данными
 
-  
-
-
-  
-
 }
 
-
-
+//2/5 продолжение удаления
+function setListener (htmlElement) {
+  htmlElement.querySelector(".elements__dell").addEventListener('click', elementDelete);
+}
+//само удаление
+function elementDelete(event) {
+  event.target.closest(".elements__item-list").remove();//найти элемент ближайщий и закрыть его
+}
 
 
 const cardElement = document.querySelector('.card');
@@ -170,6 +178,40 @@ function hendleSubmit (evt)  {
   closeCard();
 }
 
+const sizeElement  = document.querySelector(".size");
+const sizeCloseButtonElement = sizeElement.querySelector(".size__close");
+// const sizeOpenCardButton = document.querySelector("#image-element");
+
+
+
+
+let sizeText = null;//создаю переменную для перезаписи текста карточка - большая карточка
+const sizeElementText = sizeElement.querySelector("#size-txt-element");//ищу расположение текста в большом ПОПАПе.
+let sizeImg = null;
+const sizeElementImg = sizeElement.querySelector("#size-image-element");
+
+
+
+const openSize = function (eve) { //передаю эвент и открываю большую карточку
+  sizeElement.classList.add("size__open"); //делаю видимой большую карточку
+  sizeText = eve.target.closest(".elements__item-list"); //нахожу элемент по которому кликнул
+  sizeImg = eve.target.closest(".elements__item-list"); //нахожу элемент по которому кликнул
+  const text = sizeText.querySelector(".elements__cut-text").textContent; //получаю нужное значение - текст мста
+  const img = sizeImg.querySelector("#image-element").src;
+  sizeElementText.innerText = text; //вставляю текст в ПОПАП. 
+  sizeElementImg.src = img; 
+};
+
+
+
+const sizeClose = function () {
+  sizeElement.classList.remove('size__open');
+}
+
+sizeCloseButtonElement.addEventListener('click', sizeClose);
+
+
+renderCard();//в самый конец - она все и запускает автоматом.
 
 
 
@@ -179,4 +221,23 @@ function hendleSubmit (evt)  {
 
 
 
-          renderCard();//в самый конец - она все и запускает автоматом.
+// let textElement = document.querySelector(".elements__cut-text"); 
+
+
+
+///////////////////////////////////////////////////////
+// let WWWWWWWsizeText = sizeElement.querySelector("#size-txt-element");
+
+// function openSize(eve) {
+  
+//   sizeText = eve.target.closest(".elements__item-list");
+//   const text = sizeText.querySelector(".elements__cut-text").textContent;
+//   WWWWWWWsizeText.value = text;
+//   openSize2();
+
+// };
+
+// const openSize2 = function () {
+//   sizeElement.classList.add("size__open");
+  
+// };
